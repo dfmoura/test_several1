c2e4a08786491f8a4fb50ce550fee0e80b3ce591
diogomou <diogomou@gmail.com>
2025-05-14 10:23:10 -0300

    Salvando mudanças locais antes do pull

--- dart/1/mmy_project/lib/main.dart
+++ dart/1/mmy_project/lib/main.dart
@@ -98,9 +98,35 @@ void main() {
 
 
 
+    const int ITERACOES = 1000;
+    double serie = 1.0;
+    double denominador = 3.0;
+    double sinal = -1.0;
+
+    for(int i=0; i < ITERACOES; i++){
+        serie += sinal * (1 / denominador);
+        denominador += 2.0;
+        sinal *= -1.0;
+    }
+    double pi = 4 * serie;
+    print("Valor calculado de pi: $pi\n");
+
+
+
+    double nota1 = 10.0;
+    double nota2 = 7.0;
+    double media1 = calcularMedia(nota1, nota2);
+    print("A média entre $nota1 e $nota2 é: $media1 \n");
+
+
 
 }
 
+
+
+    double calcularMedia(double n1, double n2) {
+    return (n1 + n2) / 2;
+    }
 
 
     int fatorial (int n){
