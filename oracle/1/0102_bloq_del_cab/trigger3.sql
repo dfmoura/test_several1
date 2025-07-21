e112c661ff0874135fe4b14974155c36e981d4c2
diogomou <diogomou@gmail.com>
2025-07-21 13:29:08 -0300

    xxxxxx

---
+++ oracle/1/0102_bloq_del_cab/trigger3.sql
@@ -0,0 +1,37 @@
+CREATE OR REPLACE TRIGGER TRG_BLOQ_DEL_CAB_MTC
+BEFORE DELETE ON TGFCAB
+FOR EACH ROW
+DECLARE
+    v_count_nfe NUMBER;
+    v_count_inu NUMBER;
+BEGIN
+    -- Verifica se top.nfe <> 'M' para o registro atual: Convencional (Não Usa NF-e)
+    SELECT COUNT(*)
+      INTO v_count_nfe
+      FROM TGFTOP top 
+      WHERE top.dhalter = (SELECT MAX(t2.dhalter)
+                               FROM TGFTOP t2
+                              WHERE t2.codtipoper = top.codtipoper)
+       AND top.nfe <> 'M'
+       AND top.codtipoper = :OLD.CODTIPOPER;
+
+    -- Verifica se NÃO existem registros em TGFINU para o registro atual
+    SELECT COUNT(*)
+      INTO v_count_inu
+      FROM TGFINU u
+     WHERE u.codemp = :OLD.CODEMP
+       AND u.numnota = :OLD.NUMNOTA
+       AND u.serienota = :OLD.SERIENOTA;
+
+    -- Bloqueia a exclusão se a TOP for de NFe e o número da nota ainda não foi inutilizado
+    IF v_count_nfe = 0 AND v_count_nfe > 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, FC_FORMATAHTML(
+            'Exclusão não permitida.',
+            'Não permitida a exclusão devido a geração do número da nota fiscal, favor prosseguir com o faturamento ou inutilize o número e tente novamente.',
+            'Verifique o processo e tente novamente.'
+        ));
+    END IF;
+
+END;
+/
+/
